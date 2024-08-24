26d305f3ff005b617e557456b7635de2a6f6aa23
Peter De Clercq <peterdeclercq1997@outlook.com>
2024-08-24 17:00:04 +0200

    💬 Replace escaped newline characters with actual newlines

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const SCOPES = [
 
 const auth = new google.auth.JWT({
   email: process.env.GOOGLE_CLIENT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY,
+  key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   scopes: SCOPES,
 });
 
